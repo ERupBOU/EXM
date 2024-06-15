58179962e5cd899a6fb97887778567d6e82eb9d6
ERupBOU <140595493+ERupBOU@users.noreply.github.com>
2024-06-15 21:23:26 +0700

    Update EXMMobile.py

--- EXMMobile.py
+++ EXMMobile.py
@@ -7,7 +7,7 @@ from colorama import Fore, Style
 
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
-    
+clear_console()
 key = "krnl32"
 
 print(Fore.MAGENTA + Style.BRIGHT + """
